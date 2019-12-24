4da97653961b3ea252d1c4f2c52b232dc58a52c6
George Bockari <bitwhys@gmail.com>
2019-12-24 15:11:59 -0800

    feat(Book): add model Book class stub

---
+++ src/model/Book.js
@@ -0,0 +1,51 @@
+/**
+ *
+ * @param slots
+ * @constructor
+ */
+function Book(slots) {
+  this.isbn = slots.isbn
+  this.title = slots.title
+  this.year = slots.year
+}
+
+/**
+ * The collection of all Book instances managed by the application
+ * in the form of a JSON 'table'
+ */
+// TODO: Book.instances
+
+/**
+ * Load all managed Book instances from the persistent data store
+ */
+// TODO: Book.loadAll
+
+/**
+ * Save all managed Book instances to the persistent data store
+ */
+// TODO: Book.saveAll
+
+/**
+ * Crate a new Book instance
+ */
+// TODO: Book.add
+
+/**
+ * Update an existing Book instance
+ */
+// TODO: Book.update
+
+/**
+ * Delete a Book instance
+ */
+// TODO: Book.destroy
+
+/**
+ * Generate some example book records as test data
+ */
+// TODO: Book.crateTestData
+
+/**
+ * Clear the book datastore
+ */
+// TODO: Book.clearData
