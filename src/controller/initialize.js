32c9c08e0b145cb4451555a157e64f1719e31199
George Bockari <bitwhys@gmail.com>
2019-12-25 13:59:17 -0800

    feat(initialize.js): add global application namespace object

---
+++ src/controller/initialize.js
@@ -0,0 +1,5 @@
+/**
+ * @fileOverview Defining the main namespace ('personal library') and its MVC sub-namespaces
+ */
+// main namespce pl = 'personal library'
+const pl = { model: {}, view: {}, controller: {} }
